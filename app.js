8c1ec811d6026a6ac32134ae744cf8d6e5a584b6
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-02-02 11:06:22 +0530

    commit for action

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 
    app.post('/',function(req,res){
       console.log('Received the request & it is:::'+JSON.stringify(req.body));
-      if(req.body.result.action==''){
+      if(req.body.result.action=='BookFlight'){
     
      var resagent='Your Ticket has been booked ';
      console.log('request are'+resagent);
